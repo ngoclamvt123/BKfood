8021fe391133cdf045f30f5a92eab0e306c85df0
Lam Doan <ngoclamvt123@gmail.com>
2016-12-03 01:01:21 +0700

    fix url image

--- app/controllers/api/dishes_controller.rb
+++ app/controllers/api/dishes_controller.rb
@@ -1,10 +1,10 @@
 class Api::DishesController < ApiController
 
   def index
-    dishes = Dish.where(isActive: 1).select(:id, :title, :description, :price)
+    dishes = Dish.where(isActive: 1)
     list_dishes = {"foodInfo": []}
     dishes.each do |dish|
-      d = {id: dish.id, namefood: dish.title, price: dish.price, info: dish.description, linkImage: dish.img_url}
+      d = {id: dish.id, namefood: dish.title, price: dish.price, info: dish.description, linkImage: dish.img.url}
       list_dishes[:foodInfo] << d
     end
     respond_to do |format|
